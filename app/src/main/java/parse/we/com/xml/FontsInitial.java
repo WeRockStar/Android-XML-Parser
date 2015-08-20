fccc5b336512f7f07475a4dfcbf17508ef39c97e
comfreestyle <vr_freestyle@outlook.com>
2015-08-20 23:12:04 +0700

    Custom fonts and UI

--- app/src/main/java/parse/we/com/xml/FontsInitial.java
+++ app/src/main/java/parse/we/com/xml/FontsInitial.java
@@ -1,7 +1,19 @@
 package parse.we.com.xml;
 
+import android.app.*;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Kotchaphan Muangsan on 20/8/2558.
  */
-public class FontsInitial {
+public class FontsInitial extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setFontAttrId(R.attr.fontPath)
+                .setDefaultFontPath("fonts/Roboto-Light.ttf")
+                .build());
+    }
 }
--- app/src/main/java/parse/we/com/xml/MainActivity.java
+++ app/src/main/java/parse/we/com/xml/MainActivity.java
@@ -1,6 +1,7 @@
 package parse.we.com.xml;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends Activity {
     private Button btnParse;
@@ -49,29 +52,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        new DownloadData().execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        new DownloadData().execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml");
     }
 
     private class DownloadData extends AsyncTask<String, Void, String> {
@@ -144,5 +125,10 @@ public class MainActivity extends Activity {
         protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
         }
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
