11eedb56edbc89180bbc65803f3c333e813edf1a
comfreestyle <vr_freestyle@outlook.com>
2015-08-20 21:18:49 +0700

    Add Get and Set method

--- app/src/main/java/parse/we/com/xml/Application.java
+++ app/src/main/java/parse/we/com/xml/Application.java
@@ -4,4 +4,32 @@ package parse.we.com.xml;
  * Created by Kotchaphan Muangsan on 20/8/2558.
  */
 public class Application {
+
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
